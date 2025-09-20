8ce6cf08b539a7b5a249b9f968e410f511dd2210
Alina-Soltanici <soltanicialina777@gmail.com>
2025-09-20 17:43:05 +0300

    feat:add login abstract method

--- src/main/java/com/clinic/medAsist/service/UserService.java
+++ src/main/java/com/clinic/medAsist/service/UserService.java
@@ -1,9 +1,12 @@
 package com.clinic.medAsist.service;
 
+import com.clinic.medAsist.dto.SigninRequest;
+import com.clinic.medAsist.dto.SigninResponse;
 import com.clinic.medAsist.dto.SignupRequest;
 import com.clinic.medAsist.dto.SignupResponse;
 
 public interface UserService {
    SignupResponse signUp(SignupRequest signupRequest);
+   SigninResponse login(SigninRequest signinRequest);
 
 }
