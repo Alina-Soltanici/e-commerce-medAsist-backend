1d033cdad54e6b05d4f2f1c331a390a1369d5f9d
Alina-Soltanici <soltanicialina777@gmail.com>
2025-09-22 22:15:26 +0300

    Create UserDTO for exposing safe user data in responses

---
+++ src/main/java/com/clinic/medAsist/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.clinic.medAsist.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class UserDTO {
+    private Long id;
+    private String fullName;
+    private String email;
+}
